0d7c3953a4978cde07b9e71864d94b9ef4d70655
Made By IToncek <itoncek3@itoncek.space>
2025-05-17 18:53:07 +0200

    moving to UUIDs instead of int ids

--- src/main/java/space/itoncek/trailcompass/commons/objects/Player.java
+++ src/main/java/space/itoncek/trailcompass/commons/objects/Player.java
@@ -1,6 +1,7 @@
 package space.itoncek.trailcompass.commons.objects;
 
 import java.io.Serializable;
+import java.util.UUID;
 
-public record Player(int id, String nickname, byte[] passwordHash, boolean admin) implements Serializable {
+public record Player(UUID id, String nickname, byte[] passwordHash, boolean admin) implements Serializable {
 }
--- src/main/java/space/itoncek/trailcompass/commons/requests/auth/ProfileOtherRequest.java
+++ src/main/java/space/itoncek/trailcompass/commons/requests/auth/ProfileOtherRequest.java
@@ -4,6 +4,7 @@ import space.itoncek.trailcompass.commons.exchange.Authorized;
 import space.itoncek.trailcompass.commons.objects.Token;
 
 import java.io.Serializable;
+import java.util.UUID;
 
-public record ProfileOtherRequest(Token token, int id) implements Serializable, Authorized {
+public record ProfileOtherRequest(Token token, UUID id) implements Serializable, Authorized {
 }
