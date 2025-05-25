bb2e3ddf49ea8a5a844f28e41eb78967102328af
Made By IToncek <itoncek3@itoncek.space>
2025-05-25 20:09:33 +0200

    v0.0.9 release Updating versioning system

--- src/main/java/space/itoncek/trailcompass/commons/responses/system/ServerVersionResponse.java
+++ src/main/java/space/itoncek/trailcompass/commons/responses/system/ServerVersionResponse.java
@@ -5,7 +5,7 @@ import space.itoncek.trailcompass.commons.Constants;
 import java.io.Serial;
 import java.io.Serializable;
 
-public record ServerVersionResponse(String ver) implements Serializable {
+public record ServerVersionResponse(String ver, long appVersion) implements Serializable {
 	@Serial
 	private static final long serialVersionUID = Constants.serialVersionUID;
 }
