70f6cc84bc406cc47717178731358c12187f8473
Made By IToncek <itoncek3@itoncek.space>
2025-05-26 20:13:09 +0200

    Making Constants non-serializable

--- src/main/java/space/itoncek/trailcompass/commons/Constants.java
+++ src/main/java/space/itoncek/trailcompass/commons/Constants.java
@@ -1,9 +1,5 @@
 package space.itoncek.trailcompass.commons;
 
-import java.io.Serial;
-import java.io.Serializable;
-
-public class Constants implements Serializable {
-	@Serial
+public class Constants {
 	public static final long serialVersionUID = 2L; /* TODO)) +1 when commiting! Do not remove this! */
 }
