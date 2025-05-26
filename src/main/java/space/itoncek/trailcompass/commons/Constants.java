32277b9c823aaccdd7c3f0bf8716aab909bf83dc
Made By IToncek <itoncek3@itoncek.space>
2025-05-26 19:02:14 +0200

    (adding to v 0.0.0.9, updating serialversionuid)

--- src/main/java/space/itoncek/trailcompass/commons/Constants.java
+++ src/main/java/space/itoncek/trailcompass/commons/Constants.java
@@ -1,5 +1,9 @@
 package space.itoncek.trailcompass.commons;
 
-public class Constants {
-	public static final long serialVersionUID = 1L; /* TODO)) +1 when commiting! Do not remove this! */
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Constants implements Serializable {
+	@Serial
+	public static final long serialVersionUID = 2L; /* TODO)) +1 when commiting! Do not remove this! */
 }
