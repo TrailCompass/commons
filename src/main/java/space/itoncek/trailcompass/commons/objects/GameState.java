902e139775222efc0f9306a94f95ce16bf54beab
MadeByIToncek <itonce3@itoncek.space>
2025-05-29 18:40:37 +0200

    Removing SETUP GameState

--- src/main/java/space/itoncek/trailcompass/commons/objects/GameState.java
+++ src/main/java/space/itoncek/trailcompass/commons/objects/GameState.java
@@ -2,10 +2,9 @@ package space.itoncek.trailcompass.commons.objects;
 
 public enum GameState {
 	// TODO)) When changed, please update commons version (more info in Constants.java)
-	SETUP,
-	OUTSIDE_OF_GAME,
+	WAITING_FOR_GAME,
 	MOVE_PERIOD,
 	INGAME,
 	REST_PERIOD,
-	ERROR, ENDED
+	ERROR, ENDED;
 }
